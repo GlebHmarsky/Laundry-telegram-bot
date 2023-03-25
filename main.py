9be8fdc341b8ca88c097978927652452ba39dfca
Gleb Hmarsky <hmarsky.gleb@yandex.ru>
2023-03-25 23:06:10 +0300

    remove useless main func from main file

--- main.py
+++ main.py
@@ -9,20 +9,15 @@ logging.basicConfig(level=logging.INFO)
 TOKEN = "6135601546:AAECHTEz5rso2liRcocwAot0rXClNVs6xKk"
 
 
-def main():
-    updater = Updater(TOKEN, use_context=True)
+updater = Updater(TOKEN, use_context=True)
 
-    dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("addlaundry", add_laundry))
-    dispatcher.add_handler(CommandHandler("matchlaundry", match_laundry))
-    dispatcher.add_handler(CommandHandler("showlaundry", show_laundry))
-    dispatcher.add_handler(MessageHandler(
-        Filters.text(["White", "Colored", "Black"]), add_color))
+dispatcher = updater.dispatcher
+dispatcher.add_handler(CommandHandler("start", start))
+dispatcher.add_handler(CommandHandler("addlaundry", add_laundry))
+dispatcher.add_handler(CommandHandler("matchlaundry", match_laundry))
+dispatcher.add_handler(CommandHandler("showlaundry", show_laundry))
+dispatcher.add_handler(MessageHandler(
+    Filters.text(["White", "Colored", "Black"]), add_color))
 
-    updater.start_polling()
-    updater.idle()
-
-
-if __name__ == '__main__':
-    main()
+updater.start_polling()
+updater.idle()
