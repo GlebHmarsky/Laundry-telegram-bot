ea1d14d79c23c3516a516ac9c066bdaac923c91c
Gleb Hmarsky <hmarsky.gleb@yandex.ru>
2023-04-09 01:58:54 +0300

    MAKE A SYNCRONIZATION ABOUT GOING TO WASH!

--- data.py
+++ data.py
@@ -7,9 +7,18 @@ class Participant(TypedDict):
     ready: bool
 
 
+class ChatMessage(TypedDict):
+    chat_id: str
+    message_id: str
+
+
 class Group(TypedDict):
     main_user: str
-    participants: List[Participant]
+# TODO: change it for DICT!
+    participants: List[Participant] 
+    sent_messages: List[ChatMessage]
+    color: str
+    notification_msg: str
 
 
 ParticipantGroups = List[Group]
@@ -18,10 +27,16 @@ ParticipantGroups = List[Group]
 '''
 {
   main_user: str
+  color: str
+  notification_msg: str
   participants: {
     user_id: str;
     quantity: int;
     ready: bool;
+  }[]
+  sent_messages:   {
+    chat_id: str
+    message_id: str
   }[]
 }
 '''
--- handlers.py
+++ handlers.py
@@ -1,8 +1,9 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import CallbackContext
+from typing import List
 
 from storage import load_data, save_data
-from data import users_laundry, groups_by_main_user
+from data import users_laundry, groups_by_main_user, groups_washing, Participant
 
 
 def start(update: Update, context: CallbackContext):
@@ -208,6 +209,13 @@ def send_color_selection_message(update, context, laundry_groups):
     )
 
 
+def create_participants(laundry_group):
+    participants: List[Participant] = []
+    for user_id in laundry_group:
+        participants.append({"ready": None, "quantity": 1, "user_id": user_id})
+    return participants
+
+
 def color_selected(update: Update, context: CallbackContext):
     query = update.callback_query
     main_user_id = str(query.from_user.id)
@@ -217,9 +225,18 @@ def color_selected(update: Update, context: CallbackContext):
 
     laundry_group = groups_by_main_user[main_user_id][selected_color]
 
+    participants = create_participants(laundry_group)
+    groups_washing.append({
+        "main_user": main_user_id,
+        "participants": participants,
+        "sent_messages": [],
+        "color": selected_color,
+        "notification_msg": "hemlo this is notification_msg"
+    })
+
     for user_id in laundry_group:
-        send_other_participants(
-            selected_color, laundry_group, user_id, context=context)
+        send_other_participants(main_user_id,
+                                selected_color, participants, user_id, context=context)
 
 
 def match_laundry(update: Update, context: CallbackContext):
@@ -252,11 +269,6 @@ def match_laundry(update: Update, context: CallbackContext):
 
     if matched_colors:
         response = "You have matched laundry groups for the following colors:"
-        # for color in matched_colors:
-        #     response += f"{color}\n"
-        # update.message.reply_text(response)
-
-        # response = "Users by color:"
         for color in laundry_groups:
             users_group = laundry_groups[color]
             response += f"\n\nColor: {color} ->"
@@ -268,32 +280,110 @@ def match_laundry(update: Update, context: CallbackContext):
         send_color_selection_message(update, context, laundry_groups)
 
         groups_by_main_user[main_user_id] = laundry_groups
-    # Sending messages
-    # for color, users_group in laundry_groups.items():
-    #     for user_id in users_group:
-    #         send_other_participants(
-    #             color, users_group, user_id, context=context)
 
     else:
         update.message.reply_text(
             "No matches found for your laundry. Add more items with /addlaundry or wait for others to join.")
 
 
-def send_other_participants(color, users_group, send_user_id, context: CallbackContext):
+def handle_yes_no_button(update: Update, context: CallbackContext):
+    query = update.callback_query
+    user_id = query.from_user.id
+    data = query.data.split(':')
+    choice, selected_user_id = data[0], data[1]
+    group = find_group_by_main_user(groups_washing, selected_user_id)
+
+    if not group:
+        return
+
+    specific_participant = find_user_by_id(group["participants"], str(user_id))
+    if specific_participant:
+        specific_participant["ready"] = choice == "yes"
+    else:
+        print(
+            f"there is {user_id} ({specific_participant}) goes for undefined")
+    # Update the message text for other users
+    text_message = create_text_to_participants(
+        group["color"], group["participants"], context)
+
+    for sent_message in group["sent_messages"]:
+        chat_id = sent_message["chat_id"]
+        message_id = sent_message["message_id"]
+
+        # Update the message in each chat
+        context.bot.edit_message_text(chat_id=chat_id, message_id=message_id,
+                                      text=text_message, reply_markup=create_yes_no_keyboard(selected_user_id))
+
+    query.answer()
+
+
+def create_yes_no_keyboard(user_id):
+    keyboard = [
+        [
+            InlineKeyboardButton("Yes", callback_data=f"yes:{user_id}"),
+            InlineKeyboardButton("No", callback_data=f"no:{user_id}")
+        ]
+    ]
+    return InlineKeyboardMarkup(keyboard)
+
+
+def find_group_by_main_user(groups, main_user_to_find):
+    for group in groups:
+        if group["main_user"] == main_user_to_find:
+            return group
+    return None
+
+# TODO: change it for DICT!
+
+
+def find_user_by_id(list, user_to_find):
+    for part in list:
+        if part["user_id"] == user_to_find:
+            return part
+    return None
+
+
+def create_text_to_participants(color, participants: List[Participant], context: CallbackContext):
+    emoji_check = "\U00002705"
+    emoji_cross = "\U0000274C"
+    emoji_thinks = "\U0001F4AD"
+
     text_message = "Test, don't worry - be happy ^_^ \n"
 
     text_message += f"\nThere is a match on color: \"{color}\" with ->"
-    for user_id in users_group:
-        user_chat = context.bot.get_chat(user_id)
+    for participant in participants:
+        user_chat = context.bot.get_chat(participant["user_id"])
         text_message += "\n"
+        ready = participant["ready"]
+        if ready is None:
+            text_message += emoji_thinks
+        elif ready:
+            text_message += emoji_check
+        else:
+            text_message += emoji_cross
+
         if user_chat.first_name != "":
             text_message += f"{user_chat.first_name} "
         if user_chat.last_name != "":
             text_message += f"{user_chat.last_name} "
 
         text_message += f"(@{user_chat.username})"
+    return text_message
 
-    context.bot.send_message(send_user_id, text_message)
+
+def send_other_participants(main_user_id, color, users_group, send_user_id, context: CallbackContext):
+    text_message = create_text_to_participants(color, users_group, context)
+
+    message = context.bot.send_message(chat_id=send_user_id, text=text_message,
+                                       reply_markup=create_yes_no_keyboard(main_user_id))
+
+    group = find_group_by_main_user(groups_washing, main_user_id)
+
+    if not group:
+        return
+
+    group["sent_messages"].append(
+        {"chat_id": send_user_id, "message_id": message.message_id})
 
 
 def add_id_to_array(dictionary, key, id_value):
