88ed36bedbc7e259a3ff0ac49b63dad146c6438f
Gleb Hmarsky <hmarsky.gleb@yandex.ru>
2023-04-09 00:06:47 +0300

    Add new type

--- data.py
+++ data.py
@@ -1,3 +1,30 @@
-# Stores users' laundry info: {user_id: {"white": 0, "color": 0, "black": 0}}
+from typing import List, TypedDict
+
+
+class Participant(TypedDict):
+    user_id: str
+    quantity: int
+    ready: bool
+
+
+class Group(TypedDict):
+    main_user: str
+    participants: List[Participant]
+
+
+ParticipantGroups = List[Group]
+
+
+'''
+{
+  main_user: str
+  participants: {
+    user_id: str;
+    quantity: int;
+    ready: bool;
+  }[]
+}
+'''
+groups_washing: ParticipantGroups = []
+
 users_laundry = {}
-# laundry_groups = {"white": [], "colored": [], "black": []}
